56f64ce759eb78135ee70997b4fee37e41559a92
Ruben Matamoros <ruben@valiantcreative.net>
2021-05-10 23:09:06 -0400

    initial commit with structure

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'Valiant Creative',
+  shortName: 'Valiant Creative',
+  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
+  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'Ruben Matamoros',
+  github: 'valiantcreative33'
+}
