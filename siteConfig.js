bc5caea73534c97607e8572be122ddec4626f6b1
valiantcreative33 <ruben@valiantcreative.net>
2021-09-30 00:47:35 -0400

    testing a different website title

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: 'Valiant Creative',
-  shortName: 'Valiant Creative',
-  description: 'A highly motivated Graphic & Web Design professional with 10 plus years experience, now focusing on Front End Web Development.',
-  url: 'https://gatsby-london.netlify.com', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Ruben Matamoros',
-  github: 'valiantcreative33'
-}
+  name: "Valiant Creative",
+  shortName: "React Portfolio",
+  description:
+    "A highly motivated Graphic & Web Design professional with 10 plus years experience, now focusing on Front End Web Development.",
+  url: "https://gatsby-london.netlify.com", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Ruben Matamoros",
+  github: "valiantcreative33"
+};
